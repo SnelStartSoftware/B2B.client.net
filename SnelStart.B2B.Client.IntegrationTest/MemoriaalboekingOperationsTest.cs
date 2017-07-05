8cf3a367c4ebeba7daae7da5bf3213fa1f907917
swdevss <swouda@snelstart.nl>
2017-07-05 17:09:45 +0200

    Added integrationTest with empty model

---
+++ SnelStart.B2B.Client.IntegrationTest/MemoriaalboekingOperationsTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using SnelStart.B2B.Client.Operations;
+
+namespace SnelStart.B2B.Client.IntegrationTest
+{
+    [TestFixture]
+    public class MemoriaalboekingOperationsTest : CrudTest<MemoriaalboekingModel>
+    {
+        private B2BClient _client;
+
+        [SetUp]
+        public void Setup()
+        {
+            _client = DependencyRoot.Client;
+        }
+        protected override ICrudOperations<MemoriaalboekingModel> CrudSubject => _client.Memoriaalboekingen;
+        protected override MemoriaalboekingModel CreateNewModel()
+        {
+            return new MemoriaalboekingModel
+            {
+          
+            };
+        }
+    }
+}
