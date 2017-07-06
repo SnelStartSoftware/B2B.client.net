39af2b2de7ea2d8553427c9415aa88608dfdebe5
swdevss <swouda@snelstart.nl>
2017-07-06 11:17:44 +0200

    Added GrootboekenOperations

--- SnelStart.B2B.Client/IB2BClient.cs
+++ SnelStart.B2B.Client/IB2BClient.cs
@@ -7,5 +7,6 @@ namespace SnelStart.B2B.Client
     {
         IAuthenticationOperations Authentication { get; }
         IKostenplaatsenOperations Kostenplaatsen{ get; }
+        IGrootboekenOperations Grootboeken { get; }
     }
 }
