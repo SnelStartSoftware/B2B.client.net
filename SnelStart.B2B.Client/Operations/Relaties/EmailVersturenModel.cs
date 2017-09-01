9cfda63f81fd24a3051acf2e5991535f432ed8ba
Floris <floris.vaandrager@perfectview.nl>
2017-09-01 15:10:00 +0200

    Relaties operations added

---
+++ SnelStart.B2B.Client/Operations/Relaties/EmailVersturenModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SnelStart.B2B.Client.Operations
+{
+    public class EmailVersturenModel
+    {
+        string CcEmail { get; set; }
+        string Email { get; set; }
+        bool ShouldSend { get; set; }
+    }
+}
