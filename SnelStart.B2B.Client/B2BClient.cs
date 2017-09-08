91f7e9384a8bec6dec8f4849a80a4f3bdb4a7d12
Floris <floris.vaandrager@perfectview.nl>
2017-09-08 15:20:07 +0200

    - added ConfigureAwait in all awaited calls, so I don't lose context. - made clientState public, so I can use the returned access token

--- SnelStart.B2B.Client/B2BClient.cs
+++ SnelStart.B2B.Client/B2BClient.cs
@@ -6,7 +6,7 @@ namespace SnelStart.B2B.Client
 {
     public class B2BClient : IB2BClient
     {
-        private readonly ClientState _clientState;
+        public readonly ClientState clientState;
 
         public IAuthenticationOperations Authentication { get; }
         public IKostenplaatsenOperations Kostenplaatsen { get; }
@@ -15,6 +15,7 @@ namespace SnelStart.B2B.Client
         public IMemoriaalboekingenOperations Memoriaalboekingen { get; }
         public IDagboekenOperations Dagboeken { get; }
         public IRelatiesOperations Relaties { get; }
+        public IVerkoopboekingenOperations Verkoopboekingen { get; }
 
         public B2BClient(Config config)
         {
@@ -23,30 +24,31 @@ namespace SnelStart.B2B.Client
                 throw new ArgumentNullException(nameof(config));
             }
 
-            _clientState = new ClientState(config);
-            Authentication = new AuthenticationOperations(_clientState);
-            Kostenplaatsen = new KostenplaatsenOperations(_clientState);
-            Grootboeken = new GrootboekenOperations(_clientState);
-            Landen = new LandenOperations(_clientState);
-            Memoriaalboekingen = new MemoriaalboekingenOperations(_clientState);
-            Dagboeken = new DagboekenOperations(_clientState);
-            Relaties = new RelatiesOperations(_clientState);
+            clientState = new ClientState(config);
+            Authentication = new AuthenticationOperations(clientState);
+            Kostenplaatsen = new KostenplaatsenOperations(clientState);
+            Grootboeken = new GrootboekenOperations(clientState);
+            Landen = new LandenOperations(clientState);
+            Memoriaalboekingen = new MemoriaalboekingenOperations(clientState);
+            Dagboeken = new DagboekenOperations(clientState);
+            Relaties = new RelatiesOperations(clientState);
+            Verkoopboekingen = new VerkoopboekingenOperations(clientState);
         }
 
         public async Task AuthorizeAsync()
         {
-            var pair = _clientState.Config.GetApiUsernamePassword();
+            var pair = clientState.Config.GetApiUsernamePassword();
 
-            var clientStateAccessToken = await Authentication.LoginAsync(pair.Username, pair.Password);
-            _clientState.AccessToken = clientStateAccessToken.AccessToken;
+            var clientStateAccessToken = await Authentication.LoginAsync(pair.Username, pair.Password).ConfigureAwait(false);
+            clientState.AccessToken = clientStateAccessToken.AccessToken;
 
-            _clientState.HttpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_clientState.AccessToken}");
-            _clientState.HttpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _clientState.Config.SubscriptionKey);
+            clientState.HttpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {clientState.AccessToken}");
+            clientState.HttpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", clientState.Config.SubscriptionKey);
         }
 
         public void Dispose()
         {
-            _clientState.Dispose();
+            clientState.Dispose();
         }
     }
 }
